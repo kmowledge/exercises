25e57a5f89878446c798cb9f0a0ea6c885c62234
kmowledge <116028665+kmowledge@users.noreply.github.com>
2023-09-28 13:42:19 +0200

    basic struct operations

--- studying-go/structs.go
+++ studying-go/structs.go
@@ -2,27 +2,73 @@ package main
 
 import "fmt"
 
-type Animal struct{
-	genre, class, huntedBy string
-	eats []string
-	avgLifespan int
-	gender bool
+/*
+What a struct is and how we can declare and initialize it in our Go program.
+
+How to access or assign the data of an individual struct field.
+
+How to compare one struct with another and check for equality or inequality.
+*/
+
+// declare only
+type SomeStruct struct {
+	f1 string
+	f2 int
+	f3 bool
+	f5 []string
 }
 
-//default features of crocodile(animal)
-type crocodile struct {
-	genre: "crocodile"
-	class "reptile"
-	eats {"fish", "birds", "mammals"}
-	huntedBy {"human"}
-	avgLifespan 55
-	gender bool
+// initialize through literal
+var Inst1 = SomeStruct{ //returns struct
+	f1: "",
+	f2: 10,
+	//f4: "Added feature four", No such option to initialize non-declared feature.
+	f5: make([]string, 5, 10),
 }
 
 func main() {
-	crocTom := new(crocodile) //instance of crocodile struct
-	crocTom {
-		gender: true
-	}
-	crocTom.eats{"pumpkin"} //I override the default values
+	Inst2 := new(SomeStruct)          // returns pointer to struct
+	Inst2.f1 = "feature one of Inst2" //assign
+	Inst1.f5 = append(Inst1.f5, "Added", "feature", "five", "to", "Inst1")
+	fmt.Printf("%v\n%v\n%t\n", Inst1, Inst2.f1, &Inst1 == Inst2) //access, compare
 }
+
+/*
+Want: Animal (declare feats) -> Crocodile (init default feats) -> crocTom (original feats)
+Can: Animal (declare feats) -> Crocodile (Animal) func setFeats -> instance (not working)
+Can: Animal (declare feats) -> Crocodile (Animal) var, instance.
+
+
+type Animal struct {
+	Genre, Class, huntedBy string
+	Eats                   []string
+	avgLifespan            int
+	Gender                 bool
+}
+
+// type Crocodile struct {
+// 	Animal
+// }
+
+// default features of Crocodile(animal)
+func Crocodile() Animal {
+Animal:
+	Animal{
+		Genre:       "Crocodile",
+		Class:       "reptile",
+		Eats:        []string("fish", "birds", "mammals"),
+		huntedBy:    "human",
+		avgLifespan: 55,
+		Gender:      bool,
+	}
+}
+
+func main() {
+	crocTom := Crocodile() //instance of Crocodile struct
+	crocTom{
+		Gender: true,
+	}
+	crocTom.Eats{"pumpkin"} //I override the default values
+	fmt.Printf("%#v", crocTom)
+}
+*/
