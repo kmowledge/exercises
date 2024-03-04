0695b6722ae579f6c0d6763c1afe3438751e25e1
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-03-04 22:01:11 +0100

    basic use of time lib

--- go/time+dur.go
+++ go/time+dur.go
@@ -9,9 +9,9 @@ func main() {
 	var a time.Time
 	var b time.Duration
 	var aa string = "13:25:00"
+	a, _ = time.Parse(time.TimeOnly, aa)
 	var bb string = "2h30min05s"
 	b, _ = time.ParseDuration(bb)
-	a, _ = time.Parse(time.TimeOnly, aa)
 	c := a.Add(b)
 	fmt.Println(c.Format(time.TimeOnly))
 }
