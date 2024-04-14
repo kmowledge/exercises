6d60753459675a9fde0280503bf9aeb812e7f14c
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-04-14 22:45:24 +0200

    Identified button in html

--- python/beautifulsoup.select.py
+++ python/beautifulsoup.select.py
@@ -1,7 +1,9 @@
 # This might be universal script for scraping series of articles, like tutorials, but I would need
-# to find more universal identifiers for text. Probably even change method for more appropriate.
+# to find more universal html,css identifiers for text. Probably even search for completely another method.
 
 import bs4, requests
+from selenium import webdriver
+
 """
 res = requests.get('https://quii.gitbook.io/learn-go-with-tests#read-the-book')
 
@@ -30,8 +32,8 @@ with open('Manual,test-driven-go.html') as file:
       soup = bs4.BeautifulSoup(file, features="html.parser")
 
 # (manually) Inspect the site for text elements. What's the mutual value?
-# <p class="max-w-3xl w-full mx-auto decoration-primary/6 page-api-block:ml-0">
-# h1,h2,h3 etc. and p elements
+# -> class value of p element: <p class="max-w-3xl w-full mx-auto decoration-primary/6 page-api-block:ml-0">
+# -> h1,h2,h3 etc. + p elements
 
 # Select html elements with the deserved text content. Write text of these elements into destination file.
 
@@ -45,12 +47,29 @@ for elem in elems: # type(elem) <class 'bs4.element.Tag'>
 destinFile.close()
 
 # (manually) Check the result. Open the file with scraped text of 1st chapter.
-# Paragraphs are not maintained, one long string, and missing. Tried to prettify() the html before str().
-
+#-> Paragraphs are not maintained, one long string, and cull. Tried to prettify() the html before str().
 
 # (manually) Inspect the 1. and 2. chapter's site for the button to move to the next one.
+#-> last link on the page containing path: learn-go-with-tests
+
 # Select html element that indicates it's the next chapter button. Click on it. Handle exception.
 # OR derive the link url from button element and use it (change site without clicking the button).
+
+browser = webdriver.Firefox()
+browser.get('https://quii.gitbook.io/learn-go-with-tests#read-the-book')
+
+
+
+elems2 = ''
+try:
+      elems2 = browser.find_elements_by_partial_link_text('learn-go-with-tests')
+except:
+      print("Couldn't find element: %s" %elems2)
+
+elems2[-1].click()
+
+print(browser.current_url)
+
 # Contain the actions (scraping, clicking button) in the loop, until there's no button identifier found.
 # (manually) Check the result.
 # Polish the code, maybe it needs one blank line between chapters for readibilty.
