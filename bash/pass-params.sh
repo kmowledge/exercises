303c04d30cedade15484d58db54239c18a888271
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-04-22 15:09:50 +0200

    func def, call; details: "$1", ${1}123

--- bash/pass-params.sh
+++ bash/pass-params.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
+#function definition
+pass_args() {
 echo "You provided $# facts about yourself!"
 echo "Your name is $1."
 echo "Your age is $2."
+echo "${1}123 is a weak password"
+}
+
+#function call
+pass_args "$1" $2
+#If you pass the name argument as 'name surname' it will be caught as 1 param only via:  "$1"
