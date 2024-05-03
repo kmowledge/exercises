8de1390af5703cba6229e42b782cd2f564621269
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-05-03 11:39:19 +0200

    completed.bug: instance-specific property is inaccessible for method

---
+++ javascript/priv_props.js
@@ -0,0 +1,44 @@
+// team_score and personal_score are private properties, and getScore() is a privileged method.
+// team_score is a shared variable to all the Player instances.
+
+var Player = (function() {
+      var team_score = 0;
+
+      function innerPlayer(name) {
+            this.name = name; // public property
+            personal_score = 0; // private property
+      }
+
+      innerPlayer.prototype.score = function() {
+            team_score += 1;
+            personal_score += 1;
+      };
+
+      // innerPlayer.prototype.personal_score = 0
+
+      innerPlayer.prototype.getScore = function(arg) {
+            if (arg == 'team') {
+                  return team_score;
+            } else if (arg == 'personal') {
+                  return `${this.name} scored ${personal_score}.`;
+            } else {
+                  return `Personal: ${this.personal_score} | Team: ${team_score}`;
+            }
+      };
+
+      return innerPlayer
+}());
+
+player1 = new Player("Tobiasz");
+player2 = new Player("Piotr");
+
+console.log(player1.name) // "Tobiasz"
+console.log(player1.personal_score) // undefined
+console.log(player1.team_score) // undefined
+
+console.log(player1.getScore()); // undefined (no parameter provided)
+player1.score();
+player2.score();
+player2.score();
+console.log(player1.getScore('team')); // 3
+console.log(player2.getScore('personal')) // 2
