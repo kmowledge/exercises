b866aa395912c917cf6834169c2636812481befc
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-05-16 19:24:23 +0200

    I can't apply regex to dict. dict->str OK, but can I str -> dict?

--- python/json_manipulation.py
+++ python/json_manipulation.py
@@ -10,7 +10,12 @@ import re
 with open("./ins/blik.json", "r") as read_file:
       data = json.load(read_file)
 
-re.findall("\{"localTraceId" }")
-{\s[ \t]+\"localTraceId..+,
-"{\'localTraceId'\:.+?\'query\':.+?\'variables\':.+?\'response\':.+?}},"g
+print(type(data))
+str_data = str(data)
+print(type(str_data))
+
+zwrotki = re.findall(r"{\'localTraceId'\:.+?\'query\':.+?\'variables\':.+?\'response\':.+?}},", str_data)
+print(zwrotki[0])
+dict_data = dict(enumerate(str_data))
+print(dict_data) # out >> {0: '{', 1: "'", 2: 'r', 3: 'e', 4: 'q', 5: 'u', 6: 'e', 7: 's', 8: 't',
 # for chunk in "},".split()
