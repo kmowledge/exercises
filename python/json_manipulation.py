aa6483acec13cc634dc520d8b17c0c9e1ab679c7
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-05-16 21:10:50 +0200

    Test: identified false-positives from the 'query items' regex

--- python/json_manipulation.py
+++ python/json_manipulation.py
@@ -15,12 +15,24 @@ str_data = str(data)
 print(type(str_data))
 
 str_zwrotki = re.findall(r"{\'localTraceId'\:.+?\'query\':.+?\'variables\':.+?\'response\':.+?}},", str_data)
-print(str_zwrotki[0])
+# print(str_zwrotki[0])
 
 
-zwrotki = re.findall(r"('\w+'\:\s[\'\{][\w\W]*?[\'\}],\s*)"g, str_data)
-'\w+'\:\s[\'\{][\w\W]*?[\'\}],\s* # matches items on query level
+zwrotki = re.findall(r"('\w+'\:\s[\'\{][\w\W]*?[\'\}],?\s*)", str_data)
+# '\w+'\:\s[\'\{][\w\W]*?[\'\}],\s* # matches items on query level
+[print(z) for z in zwrotki]
+print('\n\n\n\n')
+print(zwrotki)
+print('\n\n\n\n')
+print(data)
 
+''' Problems:
+1. nested dicts are cut.
+'variables': {'
+'response': {'
 
+2. one nested item from 'variables' is listed on query level.
+'type': 'blik'
+'''
 
 # how to set option: dot matches all? in case a need to apply pattern also to JSON (with \t \n).
